6ef8b0361b21dd4afa9af7a5ec5654a74023f11b
Will <FernandesWilliamProfessionnel@gmail.com>
2024-08-14 22:34:54 +0200

    add logo

--- app/layout.js
+++ app/layout.js
@@ -14,9 +14,10 @@ export default function RootLayout({children}) {
         <html lang="en" style={{colorScheme: 'dark'}} className="h-full dark">
         <head>
             <title>Willz</title>
+            <link rel="icon" type="image/x-icon" href="favicon.ico"/>
         </head>
 
-        <body className={inter.className+ '  overflow-x-hidden'}>
+        <body className={inter.className + '  overflow-x-hidden'}>
         {children}
         <Toaster />
         <footer className="w-full h-[70px] flex justify-center items-center flex-col">
--- components/nav-bar.jsx
+++ components/nav-bar.jsx
@@ -13,24 +13,27 @@ import ShineBorder from "@/components/magicui/shine-border";
 import {BorderBeam} from "@/components/magicui/border-beam";
 import {ContactDialog} from "@/components/contact-dialog";
 import ShimmerButton from "@/components/magicui/shimmer-button";
-
+import Logo from "@/public/logo2.png";
+import Image from "next/image";
 export function NavBar() {
 
     const [open, setOpen] = useState(false);
     return <nav className="flex w-full h-[80px] justify-between items-center">
-        <h1 className="text-[40px] font-bold flex flex-row">
-            <p>P</p><p className="text-secondary">ortfolio</p>
+        <h1 className="text-[40px] font-bold flex flex-row justify-center items-center">
+            <Image src={Logo} className={"size-[100px]"}/>
+
+            <p className={"max-md:opacity-0"}>Portfolio</p>
         </h1>
         <div>
             <div className={"flex items-center justify-evenly"}>
-                <div className={"relative "}>
+                <div className={"relative"}>
                     <ContactDialog
                         onClick={() => {
                             console.log("Contact Me");
                         }}
                         text={'Get in Touch'}
                                    variant={'outline'}
-                                   buttonClassName={"max-w-[80px]  md:max-w-[140px]  min-h-[46px] z-30"}/>
+                                   buttonClassName={"max-w-[100px]  md:max-w-[140px]  min-h-[46px] z-30"}/>
 
                     <BorderBeam size={250} duration={12} delay={9} className={'rounded hover:cursor-pointer'}/>
                 </div>
